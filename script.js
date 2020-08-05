b120864a90a5fcdbc5e9765b6730edd08270a817
Shaker Kattoula <smkattoula@gmail.com>
2020-08-05 15:49:03 -0700

    create cards functionality and flip effect

---
+++ script.js
@@ -0,0 +1,77 @@
+const cardsContainer = document.getElementById("cards-container");
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
+const currentEl = document.getElementById("current");
+const showBtn = document.getElementById("show");
+const hideBtn = document.getElementById("hide");
+const questionEl = document.getElementById("question");
+const answerEl = document.getElementById("answer");
+const addCardBtn = document.getElementById("add-card");
+const clearBtn = document.getElementById("clear");
+const addContainer = document.getElementById("add-container");
+
+// Create a variable to keep track of current card
+let currentActiveCard = 0;
+
+// Create an array to store DOM cards
+const cardsEl = [];
+
+// Create a variable that stores the card data
+const cardsData = [
+  {
+    question: "What is a variable?",
+    answer: "A container for a piece of data",
+  },
+  {
+    question: "What is PHP?",
+    answer: "A programming language",
+  },
+  {
+    question: "What is React?",
+    answer: "A JavaScript framework",
+  },
+];
+
+// Create a function that allows you to create all cards
+function createCards() {
+  cardsData.forEach((data, index) => createCard(data, index));
+}
+
+// Create a function that allows you to create a single card in the DOM
+function createCard(data, index) {
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  if (index === 0) {
+    card.classList.add("active");
+  }
+
+  card.innerHTML = `<div class="inner-card">
+    <div class="inner-card-front">
+      <p>
+        ${data.question}
+      </p>
+    </div>
+    <div class="inner-card-back">
+      <p>
+        ${data.answer}
+      </p>
+    </div>
+  </div>`;
+
+  card.addEventListener("click", () => card.classList.toggle("show-answer"));
+
+  // Add card to DOM cards
+  cardsEl.push(card);
+
+  cardsContainer.appendChild(card);
+
+  updateCurrentText();
+}
+
+// Create a function that updates the current number of cards
+function updateCurrentText() {
+  currentEl.innerText = `${currentActiveCard + 1}/${cardsEl.length}`;
+}
+
+createCards();
